fec6b0f3a669e10ba4339a607b3bc223c4b0b229
tdmtsCloud <tom_demets@outlook.com>
2020-04-29 11:37:07 +0200

    Add project files.

---
+++ TCPServer/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace TCPServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonServer = new System.Windows.Forms.Button();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonServer
+            // 
+            this.buttonServer.Location = new System.Drawing.Point(12, 12);
+            this.buttonServer.Name = "buttonServer";
+            this.buttonServer.Size = new System.Drawing.Size(776, 65);
+            this.buttonServer.TabIndex = 0;
+            this.buttonServer.Text = "Start Server";
+            this.buttonServer.UseVisualStyleBackColor = true;
+            this.buttonServer.Click += new System.EventHandler(this.ButtonServer_Click);
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Location = new System.Drawing.Point(12, 103);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.Size = new System.Drawing.Size(776, 335);
+            this.textBoxLog.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 454);
+            this.Controls.Add(this.textBoxLog);
+            this.Controls.Add(this.buttonServer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonServer;
+        private System.Windows.Forms.TextBox textBoxLog;
+    }
+}
+
---
+++ TCPServer/Form1.cs
@@ -0,0 +1,49 @@
+﻿using SimpleTcp;
+using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TCPServer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Globale variabele
+        TcpServer tcpServer;
+        private void ButtonServer_Click(object sender, EventArgs e)
+        {
+            //Server starten die luistert op poort 9000
+            tcpServer = new TcpServer("0.0.0.0", 9000, false, "", "");
+            //Eventhandler toevoegen die getriggerd wordt als een client data stuurt.
+            tcpServer.DataReceived += TcpServer_DataReceived; 
+            //De server starten.
+            tcpServer.Start();
+        }
+
+        private void TcpServer_DataReceived(object sender, DataReceivedFromClientEventArgs e)
+        {
+            //Data omzetten naar leesbare tekst.
+            string data = Encoding.UTF8.GetString(e.Data);
+            //ENTER weg
+            data = data.Trim(); 
+
+            //Actie ondernemen als de data een bepaalde waarde heeft.
+            if(data.Equals("abc", StringComparison.OrdinalIgnoreCase))
+            {
+
+            }
+
+            //Data tonen in textbox.
+            textBoxLog.Invoke((MethodInvoker)delegate {
+                //Op de GUI thread.
+                textBoxLog.AppendText(data);
+                textBoxLog.AppendText(Environment.NewLine);
+            });
+        }
+
+    }
+}
